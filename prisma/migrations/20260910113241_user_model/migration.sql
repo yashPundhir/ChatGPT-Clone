dd5cb657d44d88c44435980e2937479ca2637177
yashPundhir <yash.pundhir.prof@gmail.com>
2026-09-10 23:18:45 +0530

    do the following changes: create User model in schema file add auto generated migration file add sign-in detection functionality to UI

---
+++ prisma/migrations/20260910113241_user_model/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Test` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Test";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "clerkId" TEXT NOT NULL,
+    "email" TEXT,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "imageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_clerkId_key" ON "User"("clerkId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { auth } from "@clerk/nextjs/server";
+
 import { ThemeToggler } from "@/components/ui/theme-toggler";
 
-export default function Home() {
+export default async function Home() {
+  await auth.protect();
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>heading text</h1>
